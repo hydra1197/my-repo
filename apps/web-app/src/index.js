eb7bcb823d8a26928d43b57b12b1038defde739a
Khoi Nguyen <khoicoder.dn@gmail.com>
2025-04-16 21:34:58 +0700

    Remove unnecessary files

--- apps/web-app/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("@my-org/utils");
-const name = "World";
-console.log((0, utils_1.greet)(name));
